9a0cadd7c8b1613bf1584a89ccc69b9be3ab0dd4
Sandeep Mavi <smavisoftwares@gmail.com>
2024-12-11 15:06:55 +0530

    Adding ,ore Functionalities

--- react-counter-app/src/reducerCompo/Reducer.jsx
+++ react-counter-app/src/reducerCompo/Reducer.jsx
@@ -3,16 +3,16 @@ import { useReducer } from "react";
 // import React from 'react'
 
 const Reducer = () => {
-  const initialValue = 0;
+  const initialValue = {name: 'ABC',age: 25};
   const[state, dispatch] = useReducer(reducer, initialValue);
   function reducer(state, action){
-    switch(action){
-      case "add":
-        return state + 1;
-      case "subtract" :
-        return state - 1;
+    switch(action.type){
+      case "updateName":
+        return {...state,name:'XYZ'};
+      case "updateAge" :
+        return {...state, age:state.age+1};
       case "reset":
-        return 0;
+        return {name: 'ABC', age:25};
       default:
         throw new Error("unexpected action");      
     }
@@ -20,10 +20,11 @@ const Reducer = () => {
   return (
     <div>
       <h1>UseReducer Example</h1>
-      <h1>Counter:{state}</h1>
-      <button onClick={()=>dispatch("add")}>Increament</button>
-      <button onClick={()=>dispatch("subtract")}>Decreament</button>
-      <button onClick={()=>dispatch("reset")}>Reset</button>
+      <h1>name:{state.name}</h1>
+      <h1>Age:{state.age}</h1>
+      <button onClick={()=>dispatch({type:"updateName"})}>UpdateName</button>
+      <button onClick={()=>dispatch({type:"updateAge"})}>updateAge</button>
+      <button onClick={()=>dispatch({type:"reset"})}>Reset</button>
     </div>
   )
 }
